1e010016b57008284468946e0a7560a955f53a9a
laskar-ksatria <dock_fest@yahoo.co.id>
2024-03-31 23:56:42 +0700

    chore: update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
   return (
     <div className="flex md:flex-row flex-col-reverse   justify-between mb-[35px] gap-6 sticky">
       {/* <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]"> */}
-      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] rounded-[100px]">
+      <div className="lg:flex-1 flex items-center flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] rounded-[100px]">
         {/* <input
           type="text"
           placeholder="Search for campaigns"
